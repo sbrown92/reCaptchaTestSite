1715759bd1886f2d1a45b6ebbdc3cf12b8bd21c6
nickdibari <ndibari@fordham.edu>
2017-03-22 00:45:31 -0400

    Obfuscated Django secret key

--- reCap/settings.py
+++ reCap/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from keys import DJANGO_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u5==t5283+ol8lie%b#z^ca7y=@!1-u^i$dn&6=9qw4udy2i99'
+SECRET_KEY = DJANGO_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
