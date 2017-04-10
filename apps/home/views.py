49723d997cdc6ebc7117a6be6911b0dc043ac629
LuKcm888 <mmackoul@fordham.edu>
2017-04-10 00:10:13 -0400

    added function to test if recap passed or not

--- apps/home/views.py
+++ apps/home/views.py
@@ -3,17 +3,21 @@ from .forms import TestForm
 import urllib2
 import urllib
 import requests
+import re
 # Create your views here.
 
 def homepage(request):
+
+	counter = 0
 	if request.method == 'GET':
 		form = TestForm()
 
 		value = "Not Sent!"
-		return render(request, 'home/index.html', {'form': form, 'val': value})
+		return render(request, 'home/index.html', {'form': form, 'val': value, 'count': counter})
 	elif request.method == 'POST':
-		form = TestForm()
+		form = TestForm() 
 		data = request.POST
+		## gets the captcha response 
 		captcha_rs = data.get('g-recaptcha-response')
 		url = 'https://www.google.com/recaptcha/api/siteverify'
 		params = {
@@ -23,10 +27,52 @@ def homepage(request):
 
 		result = requests.post(url, params=params)
 
+		verify = recaptcha(request, data)
+
+		"""Make condition that will check to see if request is verified"""
+
+		if verify:
+
+			counter +=1
+
+			print ("SUCCESS!")
+
+		else:
+
+
+			print("FAILED TO LOG")
+
+
+
+
+			##	count 
+
+		"""If verifed, count"""
+
+		"""else: error """
+
+		"""GET DONE BY TUESDAY """
+
+
+
+
 		content = result.content
 
 
 		return render(request, 'home/result.html')
+
+
+def recaptcha(request, postdata):
+        rc_challenge = postdata.get('recaptcha_challenge_field','')
+        rc_user_input = postdata.get('recaptcha_response_field', '').encode('utf-8')
+        url = 'http://www.google.com/recaptcha/api/verify'
+        values = {'privatekey' : 'PRIVATE-KEY', 'remoteip': request.META['REMOTE_ADDR'], 'challenge' : rc_challenge, 'response' : rc_user_input,}
+        data = urllib.urlencode(values)
+        req = urllib2.Request(url, data)
+        response = urllib2.urlopen(req)
+        answer = response.read().split()[0]
+        response.close()
+        return answer
 		
 
 
