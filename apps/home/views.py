8e76b967b5291c96135784bc9bb6f9451a1edc24
sbrown92 <sbrown92@fordham.edu>
2017-04-19 10:12:39 -0400

    Removed incorrect server verification code

--- apps/home/views.py
+++ apps/home/views.py
@@ -26,54 +26,11 @@ def homepage(request):
 		}
 
 		result = requests.post(url, params=params)
-
-		verify = recaptcha(request, data)
-
-		"""Make condition that will check to see if request is verified"""
-
-		if verify:
-
-			counter +=1
-
-			print ("SUCCESS!")
-
-		else:
-
-
-			print("FAILED TO LOG")
-
-
-
-
-			##	count 
-
-		"""If verifed, count"""
-
-		"""else: error """
-
-		"""GET DONE BY TUESDAY """
-
-
-
-
+		
 		content = result.content
 
 
 		return render(request, 'home/result.html')
-
-
-def recaptcha(request, postdata):
-        rc_challenge = postdata.get('recaptcha_challenge_field','')
-        rc_user_input = postdata.get('recaptcha_response_field', '').encode('utf-8')
-        url = 'http://www.google.com/recaptcha/api/verify'
-        values = {'privatekey' : 'PRIVATE-KEY', 'remoteip': request.META['REMOTE_ADDR'], 'challenge' : rc_challenge, 'response' : rc_user_input,}
-        data = urllib.urlencode(values)
-        req = urllib2.Request(url, data)
-        response = urllib2.urlopen(req)
-        answer = response.read().split()[0]
-        response.close()
-        return answer
-		
 
 
 
