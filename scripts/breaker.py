bda68d7fb834ef67e722c3f8de9b60351b707e8a
sbrown92 <sbrown92@fordham.edu>
2017-04-27 19:28:23 -0400

    Cleaned code formatting/organization, and added some smaller functionality

--- scripts/breaker.py
+++ scripts/breaker.py
@@ -1,4 +1,5 @@
 import os.path
+import sys
 import json
 
 from bs4 import BeautifulSoup
@@ -15,6 +16,23 @@ from selenium.webdriver.common.proxy import *
 from watson_developer_cloud import SpeechToTextV1
 
 
+#######################################
+####                               ####
+####   Variable Declarations       ####
+####                               ####
+#######################################
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FIREFOX_PATH = os.path.join(BASE_DIR, 'geckodriver')
+
+
+
+#######################################
+####                               ####
+####   Functions                   ####
+####                               ####
+#######################################
+
 #Web Scraper to pull proxy server addresses and port numbers.
 def scraper():
     source = urlopen('http://proxydb.net/?protocol=https&country=US&availability=75&response_time=10')
@@ -27,75 +45,101 @@ def scraper():
             proxies.append(anchor.text.split(':'))
     return proxies
 
-Base_Dir = os.path.dirname(os.path.abspath(__file__))
-WATSON_USER = 'd136174d-690a-4baa-911d-1e759af84124'
-WATSON_PASS = 'ODA1VLDPfRX4'
+
+### Set the web browser's proxy settings.
+def getProfile(pool):
+    prefs = FirefoxProfile()
+
+    pool.pop()
+    server, host = pool.pop()
+    prefs.set_preference('network.proxy.type', 1)
+    prefs.set_preference('network.proxy.share_proxy_settings', True)
+    prefs.set_preference('network.http.use-cache', False)
+    prefs.set_preference('network.proxy.http', server)
+    prefs.set_preference('network.proxy.http_port', int(host))
+    prefs.set_preference('network.proxy.ssl', server)
+    prefs.set_preference('network.proxy.ssl_port', int(host))
+    prefs.set_preference('network.proxy.socks', server)
+    prefs.set_preference('network.proxy.socks_port', int(host))
+
+    return prefs
 
 
+def automatePage(fireFoxPath, prefs):
+    # Watson API Settings.
+    speech_engine = SpeechToTextV1(
+        username=WATSON_USER,
+        password=WATSON_PASS,
+        x_watson_learning_opt_out=True)
+    keywords = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine"
+    ]
+
+    #Automate interactions with widget.
+    #Webdriver creation
+    br = webdriver.Firefox(executable_path=fireFoxPath, firefox_profile=prefs)
+    wait = WebDriverWait(br, 5)
+    br.get(url)
+    br.find_elements_by_tag_name('iframe')
+    iframe = br.find_elements_by_tag_name('iframe')[0]
+    iframe2 = br.find_elements_by_tag_name('iframe')[1]
+    br.switch_to_frame(iframe)
+    wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'recaptcha-checkbox-checkmark'))).click()
+    br.switch_to_default_content()
+    br.switch_to_frame(iframe2)
+    wait.until(EC.presence_of_element_located((By.ID, 'recaptcha-audio-button')))
+    sleep(2)
+    wait.until(EC.element_to_be_clickable((By.ID, 'recaptcha-audio-button'))).click()
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'rc-audiochallenge-download-link')))
+    wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'rc-audiochallenge-download-link'))).click()
+
+    # gets the link fo the file
+    audiolink = br.find_elements_by_xpath("//a[@href]")
+    for link in audiolink:
+        print link.get_attribute("href")
+        finalLink = link.get_attribute("href")
+        print finalLink
+
+    #Download Audio File
+    urlretrieve(finalLink, BASE_DIR + "/" + fileName + ".mp3")
+
+    #Convert file from .mp3 to .wav
+    sx.build(fileName + ".mp3", fileName + ".wav")
+
+    #Speech To Text
+    with open(fileName + '.wav', 'rb') as sourceFile:
+        print(json.dumps(speech_engine.recognize(sourceFile,
+                                     content_type='audio/wav',
+                                     continuous=True,
+                                     model='en-UK_NarrowbandModel',
+                                     inactivity_timeout=5,
+                                     keywords=keywords,
+                                     keywords_threshold=.25)))
+
+
+#######################################
+####                               ####
+####   Variable Declarations       ####
+####                               ####
+#######################################
+
+url = raw_input("Please enter the URL: ")
+sys.stdout.write("Please enter the form inputs (id:value): ")
+sys.stdout.flush()
+formInputs = sys.stdin.readline()
 fileName = "audio"
 sx = Transformer()
-proxyList = scraper()
-prefs = FirefoxProfile()
-server, host = proxyList.pop()
-prefs.set_preference('network.proxy.type', 1)
-prefs.set_preference('network.proxy.share_proxy_settings', True)
-prefs.set_preference('network.http.use-cache', False)
-prefs.set_preference('network.proxy.http', server)
-prefs.set_preference('network.proxy.http_port', int(host))
-prefs.set_preference('network.proxy.ssl', server)
-prefs.set_preference('network.proxy.ssl_port', int(host))
-prefs.set_preference('network.proxy.socks', server)
-prefs.set_preference('network.proxy.socks_port', int(host))
-
-#Webdriver settings.
-fireFoxPath = '/Users/sam/Selenium/geckodriver'
-br = webdriver.Firefox(executable_path=fireFoxPath, firefox_profile=prefs)
+proxyPool = scraper()
+prefs = getProfile(proxyPool)
 
 
-
-#Watson API Settings.
-speech_engine = SpeechToTextV1(
-    username=WATSON_USER,
-    password=WATSON_PASS,
-    x_watson_learning_opt_out=True
-)
-
-
-#Automate interactions with widget. 
-wait = WebDriverWait(br, 5)
-br.get('http://127.0.0.1:8000')
-br.find_elements_by_tag_name('iframe')
-iframe = br.find_elements_by_tag_name('iframe')[0]
-iframe2 = br.find_elements_by_tag_name('iframe')[1]
-br.switch_to_frame(iframe)
-wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'recaptcha-checkbox-checkmark'))).click()
-br.switch_to_default_content()
-br.switch_to_frame(iframe2)
-wait.until(EC.presence_of_element_located((By.ID, 'recaptcha-audio-button')))
-sleep(1)
-wait.until(EC.element_to_be_clickable((By.ID, 'recaptcha-audio-button'))).click()
-wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'rc-audiochallenge-download-link')))
-wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'rc-audiochallenge-download-link'))).click()
-
-# gets the link fo the file
-audiolink = br.find_elements_by_xpath("//a[@href]")
-for link in audiolink:
-	#print link.get_attribute("href")
-	finalLink = link.get_attribute("href")
-	print finalLink
-
-#Download Audio File
-urlretrieve(finalLink, Base_Dir + "/" + fileName + ".mp3")
-
-#Convert file from .mp3 to .wav
-sx.build(fileName + ".mp3", fileName + ".wav")
-
-#Speech To Text
-with open(fileName + '.wav', 'rb') as sourceFile:
-    print(json.dumps(speech_engine.recognize(sourceFile,
-                                             content_type='audio/wav',
-                                             continuous=True,
-                                             model='en-UK_NarrowbandModel',
-                                             inactivity_timeout=5),
-                                             indent=2))
-
+automatePage(fireFoxPath=FIREFOX_PATH, prefs=prefs)
