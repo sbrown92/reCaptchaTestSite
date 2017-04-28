169f1190ef21eb9cd866566e9984b9fad3ca65e1
sbrown92 <sbrown92@fordham.edu>
2017-04-27 20:56:15 -0400

    Started making script fill forms

--- scripts/breaker.py
+++ scripts/breaker.py
@@ -12,19 +12,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
-from selenium.webdriver.common.proxy import *
 from watson_developer_cloud import SpeechToTextV1
 
 
 #######################################
 ####                               ####
-####   Variable Declarations       ####
+####   Constant Variables          ####
 ####                               ####
 #######################################
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 FIREFOX_PATH = os.path.join(BASE_DIR, 'geckodriver')
-
+WATSON_USER = 'd136174d-690a-4baa-911d-1e759af84124'
+WATSON_PASS = 'ODA1VLDPfRX4'
 
 
 #######################################
@@ -32,6 +32,20 @@ FIREFOX_PATH = os.path.join(BASE_DIR, 'geckodriver')
 ####   Functions                   ####
 ####                               ####
 #######################################
+
+#Get the URL and the form inputs from the user.
+def getInputs():
+    inputs = list()
+    urlAddress = raw_input("Please enter the page's URL: ")
+    sys.stdout.write("Please enter the form's inputs (formatted ID:VALUE with spaces): ")
+    sys.stdout.flush()
+    line = sys.stdin.readline()
+    for pair in line.split():
+        values = pair.split(":")
+        #TODO: Check if the id has already been set or not.
+        inputs.append((values[0], values[1]))
+
+    return urlAddress, inputs
 
 #Web Scraper to pull proxy server addresses and port numbers.
 def scraper():
@@ -65,7 +79,11 @@ def getProfile(pool):
     return prefs
 
 
-def automatePage(fireFoxPath, prefs):
+def automatePage(fireFoxPath, prefs, address, inputList):
+
+    fileName = "audio"
+    sx = Transformer()
+
     # Watson API Settings.
     speech_engine = SpeechToTextV1(
         username=WATSON_USER,
@@ -86,9 +104,24 @@ def automatePage(fireFoxPath, prefs):
 
     #Automate interactions with widget.
     #Webdriver creation
-    br = webdriver.Firefox(executable_path=fireFoxPath, firefox_profile=prefs)
+    br = webdriver.Firefox(executable_path=fireFoxPath, firefox_profile=prefs,)
     wait = WebDriverWait(br, 5)
-    br.get(url)
+    br.get(address)
+
+    ####################
+    ### Fill in Form ###
+    ####################
+
+    for input in inputList:
+        br.find_element_by_id(input[0]).text = input[1]
+
+
+
+
+
+    ########################
+    ### reCaptcha Widget ###
+    ########################
     br.find_elements_by_tag_name('iframe')
     iframe = br.find_elements_by_tag_name('iframe')[0]
     iframe2 = br.find_elements_by_tag_name('iframe')[1]
@@ -102,20 +135,21 @@ def automatePage(fireFoxPath, prefs):
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'rc-audiochallenge-download-link')))
     wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'rc-audiochallenge-download-link'))).click()
 
-    # gets the link fo the file
+    ###########################
+    ### download audio file ###
+    ###########################
     audiolink = br.find_elements_by_xpath("//a[@href]")
     for link in audiolink:
         print link.get_attribute("href")
         finalLink = link.get_attribute("href")
         print finalLink
-
-    #Download Audio File
     urlretrieve(finalLink, BASE_DIR + "/" + fileName + ".mp3")
 
-    #Convert file from .mp3 to .wav
+    ######################
+    ### Speech to Text ###
+    ######################
     sx.build(fileName + ".mp3", fileName + ".wav")
 
-    #Speech To Text
     with open(fileName + '.wav', 'rb') as sourceFile:
         print(json.dumps(speech_engine.recognize(sourceFile,
                                      content_type='audio/wav',
@@ -126,20 +160,22 @@ def automatePage(fireFoxPath, prefs):
                                      keywords_threshold=.25)))
 
 
+
+
+
 #######################################
 ####                               ####
-####   Variable Declarations       ####
+####   Main Function               ####
 ####                               ####
 #######################################
-
-url = raw_input("Please enter the URL: ")
-sys.stdout.write("Please enter the form inputs (id:value): ")
-sys.stdout.flush()
-formInputs = sys.stdin.readline()
-fileName = "audio"
-sx = Transformer()
-proxyPool = scraper()
-prefs = getProfile(proxyPool)
+def main():
+    proxyPool = scraper()
+    prefs = getProfile(proxyPool)
+    urlAddr, inputs = getInputs()
+    automatePage(fireFoxPath=FIREFOX_PATH, prefs=prefs, address=urlAddr, inputList=inputs)
 
 
-automatePage(fireFoxPath=FIREFOX_PATH, prefs=prefs)
+
+if __name__ == "__main__":
+    print "here"
+    main()
