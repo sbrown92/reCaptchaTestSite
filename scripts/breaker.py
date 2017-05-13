44589bb1e3980ff923cf6fc8056a6d7ed2138df1
sbrown92 <sbrown92@fordham.edu>
2017-05-13 19:38:03 -0400

    Fixed the timeout and widget loading error giving false sucess.

--- scripts/breaker.py
+++ scripts/breaker.py
@@ -268,10 +268,12 @@ def submitAnswer(br, answer):
 #######################################
 
 def main():
-    numCorrect = 0;
-    trials = 1;
+    numCorrect = 0
+    trials = 1
+    crashes = 0
     proxyPool = scraper()
     while trials <= 100:
+        print "Trial Number: " + str(trials)
         try:
             fileName = "audio"
             sx = Transformer()
@@ -288,6 +290,7 @@ def main():
             answer = getAnswer(fileName)
 
             if answer == '?':
+                print "Proxy was denied"
                 for proxy in proxyPool:
                     if proxy == curProxy:
                         proxy.hasFailed = True
@@ -297,6 +300,9 @@ def main():
 
         except Exception as e:
             print "Error: " + str(e)
+            trials += 1
+            crashes += 1
+            continue
 
         wait = WebDriverWait(browser, 15)
         try:
